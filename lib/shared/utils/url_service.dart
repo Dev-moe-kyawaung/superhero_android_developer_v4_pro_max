619c6dc8513fb1ac3597f65c0eda56a04c5c11c4
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-09 20:53:41 +0630

    Create url_service.dart

---
+++ lib/shared/utils/url_service.dart
@@ -0,0 +1,8 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class UrlService {
+  static Future<void> open(String url) async {
+    final uri = Uri.parse(url);
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+  }
+}
